2738669cadba244094f942f244838ce38c8b4a19
jac <crowja@gmail.com>
2020-02-17 18:10:03 -0600

    Using tinyhelp.h.

--- t/test.c
+++ t/test.c
@@ -5,45 +5,7 @@
 #include <string.h>
 #include "nsequtils.h"
 #include "tinytest.h"
-
-#ifdef COLOR_CODE
-#undef COLOR_CODE
-#endif
-#define COLOR_CODE      0x1B
-#ifdef COLOR_RED
-#undef COLOR_RED
-#endif
-#define COLOR_RED       "[1;31m"
-#ifdef COLOR_GREEN
-#undef COLOR_GREEN
-#endif
-#define COLOR_GREEN     "[1;32m"
-#ifdef COLOR_YELLOW
-#undef COLOR_YELLOW
-#endif
-#define COLOR_YELLOW    "[1;33m"
-#ifdef COLOR_RESET
-#undef COLOR_RESET
-#endif
-#define COLOR_RESET     "[0m"
-
-static void
-printf_test_name(char *name, char *info)
-{
-   printf("%c%s%s%c%s", COLOR_CODE, COLOR_YELLOW, name, COLOR_CODE, COLOR_RESET);
-
-   if (NULL != info)
-      printf(" [%s]\n", info);
-   else
-      printf("\n");
-}
-
-static int
-two_doubles_equal(double x, double y)
-{
-   double      t = fabs(x) + fabs(y);
-   return fabs(x - y) < 4 * DBL_EPSILON * t ? 1 : 0;
-}
+#include "tinyhelp.h"
 
 static void
 test_clean(void)
@@ -53,8 +15,7 @@ test_clean(void)
    char        bu[] = "ACGTAAAG";
    char        bl[] = "acgtaaag";
 
-   printf_test_name("test_clean", "nsequtils_clean");
-
+   fprintf_test_info(stdout, "test_clean", "nsequtils_clean");
    nsequtils_clean(a, 0);
    ASSERT_EQUALS(0, strcmp(a, b0));
    nsequtils_clean(a, 1);
@@ -63,12 +24,11 @@ test_clean(void)
    ASSERT_EQUALS(0, strcmp(a, bl));
 }
 
-/* 7 yy */
+/* 6 yy */
 static void
 test_stub(void)
 {
-   ASSERT("test 1 in test_stub", two_doubles_equal(0.0, 0.0));
-   printf_test_name("test_stub", NULL);
+   fprintf_test_info(stdout, "test_stub", NULL);
    ASSERT_EQUALS(0, 0);
 }
 
@@ -78,6 +38,5 @@ main(void)
    printf("%s\n", nsequtils_version());
    test_stub();                                  /* only to quiet compiler warnings */
    RUN(test_clean);
-
    return TEST_REPORT();
 }
